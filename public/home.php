f2d14b6bc9de66675dcfc58e9ef8d3c12c160601
DrWisWis <ibiapinoigor@gmail.com>
2025-10-23 08:53:18 -0300

    link css home

--- public/home.php
+++ public/home.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home</title>
-    <link rel="stylesheet" href="/src/assets/pages/Home/style.css">
+    <link rel="stylesheet" href="../src/assets/css/home.css">
     <script src="./carrosel.js" defer></script>
 </head>
 <body>
